2ca2e6e2c53b1101b7ea64b601a6c178db4db4cc
adcameron <andrew.cameron13@gmail.com>
2022-02-18 10:19:18 +1100

    Modified jobstate_query code to run somewhat faster, although it has less options. May modify in future to report all job states on a single PSRDB pass but for now this will be enough.

--- misc_scripts/jobstate_query.py
+++ misc_scripts/jobstate_query.py
@@ -21,6 +21,7 @@ import numpy as np
 
 # PSRDB imports
 from tables import *
+from joins import *
 from graphql_client import GraphQLClient
 from db_utils import (check_response, check_pipeline, get_pulsar_id, get_observation_target_id, check_pulsar_target,
                       get_job_state, get_target_name, utc_psrdb2normal, get_observation_utc)
@@ -33,9 +34,8 @@ parser = argparse.ArgumentParser(description="Reports the job state of all proce
 parser.add_argument("-outdir", dest="outdir", help="Directory in which to store the output file.", default=None)
 parser.add_argument("-outfile", dest="outfile", type=str, help="File in which to store the recalled job state results.", default=None)
 parser.add_argument("-state", dest="state", type=str, help="Return processings matching this state.", default = None)
-parser.add_argument("-pipe_id", dest="pipe_id", type=int, help="Return only those processings matching this pipeline ID.", default=None)
-parser.add_argument("-parent_id", dest="parent_id", type=int, help="Return only those processings matching this parent pipeline ID.", default=None)
-parser.add_argument("-psr", dest="pulsar", type=str, help="Return only those processings matching this PSR J-name.", default=None)
+#parser.add_argument("-pipe_id", dest="pipe_id", type=int, help="Return only those processings matching this pipeline ID.", default=None)
+#parser.add_argument("-psr", dest="pulsar", type=str, help="Return only those processings matching this PSR J-name.", default=None)
 args = parser.parse_args()
 
 
@@ -54,41 +54,45 @@ if not (args.outdir == None or args.outfile == None):
         os.makedirs(args.outdir)
 
 # input verification
-if not (args.pipe_id == None):
-    if not(check_pipeline(args.pipe_id, client, url, token)):
-        raise Exception("Invalid pipeline ID specified - aborting.")
+#if not (args.pipe_id == None):
+#    if not(check_pipeline(args.pipe_id, client, url, token)):
+#        raise Exception("Invalid pipeline ID specified - aborting.")
 
-if not (args.parent_id == None):
-    if not(check_pipeline(args.parent_id, client, url, token)):
-        raise Exception("Invalid parent pipeline ID specified - aborting.")
-
-if not (args.pulsar == None):
-    pulsar_id = get_pulsar_id(args.pulsar, client, url, token)
-    if (pulsar_id == None):
-        raise Exception("Pulsar J-name not found in the database - aborting.")
+#if not (args.pulsar == None):
+#    pulsar_id = get_pulsar_id(args.pulsar, client, url, token)
+#    if (pulsar_id == None):
+#        raise Exception("Pulsar J-name not found in the database - aborting.")
 
 # query and return all processings matching the specified parameters
 
-processings = Processings(client, url, token)
-processings.set_field_names(True, False)
-processings.get_dicts = True
-processings.set_use_pagination(True)
+#processings = Processings(client, url, token)
+#processings.set_field_names(True, False)
+#processings.get_dicts = True
+#processings.set_use_pagination(True)
 
-print ("Compiling raw list of processing entries...")
-proc_data = processings.list(None, None, args.parent_id, None, None)
+processedobservations = ProcessedObservations(client, url, token)
+# processedobservations.set_field_names(True, False)
+processedobservations.get_dicts = True
+processedobservations.set_use_pagination(True)
+
+print ("Compiling raw list of processing entries (this will take a few minutes)...")
+#proc_data = processings.list(None, None, None, None, None)
+proc_data = processedobservations.list(None, None, None, None, None)
 print ("Raw data compiled - {0} processing entries found".format(len(proc_data)))
 
 results_list = []
 
-# nothing do to but scroll
+# nothing do to but scroll - make as few PSRDB calls as possible
 
 for x in range(0, len(proc_data)):
 
     proc_entry = proc_data[x]['node']
-    proc_id = processings.decode_id(proc_entry['id'])
+    #proc_id = processings.decode_id(proc_entry['id'])
+    proc_id = processedobservations.decode_id(proc_entry['id'])
+    # can't avoid calling for the decode, hopefully this is quick
 
     # check if the job state matches
-    job_state_json = get_job_state(proc_id, client, url, token)
+    job_state_json = json.loads(proc_entry['jobState'].replace("'", '"'))
     if "job_state" in job_state_json.keys():
         job_state = job_state_json['job_state']
     else:
@@ -99,29 +103,37 @@ for x in range(0, len(proc_data)):
             continue
 
     # check if the pipeline matches
-    proc_pipe_id = int(processings.decode_id(proc_entry['pipeline']['id']))
-    if not (args.pipe_id == None):
-        if not (args.pipe_id == proc_pipe_id):
-            continue
+    #if not (args.pipe_id == None):
+        #proc_pipe_id = int(processings.decode_id(proc_entry['pipeline']['id']))
+        #if not (args.pipe_id == proc_pipe_id):
+            #continue
 
     # check if the pulsar name matches
-    obs_id = int(processings.decode_id(proc_entry['observation']['id']))
-    target_id = get_observation_target_id(obs_id, client, url, token)
-    if not (args.pulsar == None or pulsar_id == None):
-        if not (check_pulsar_target(pulsar_id, target_id, client, url, token)):
-            continue
+    #if not (args.pulsar == None or pulsar_id == None):
+        #obs_id = int(processings.decode_id(proc_entry['observation']['id']))
+        #target_id = get_observation_target_id(obs_id, client, url, token)
+        #if not (check_pulsar_target(pulsar_id, target_id, client, url, token)):
+            #continue
 
     # if we have survived this far, we have a match
     # collect the output info and add to the results list
-    target_name = get_target_name(target_id, client, url, token)
-    obs_utc = utc_psrdb2normal(get_observation_utc(obs_id, client, url, token))
-    results_list.append([proc_id, target_name, obs_id, obs_utc, job_state])
+    #target_name = get_target_name(target_id, client, url, token)
+    #obs_utc = utc_psrdb2normal(get_observation_utc(obs_id, client, url, token))
+    #results_list.append([proc_id, target_name, obs_id, obs_utc, job_state])
+
+    target_name = proc_entry['observation']['target']['name']
+    obs_utc = utc_psrdb2normal(proc_entry['observation']['utcStart'])
+    results_list.append([proc_id, target_name, obs_utc, job_state])
+
+    #results_list.append([proc_id, job_state])
 
 # write the results to file, if there are any
 
 if (len(results_list) > 0):
 
-    header = "# ProcID Target ObsID ObsUTC JobState"
+    #header = "# ProcID Target ObsID ObsUTC JobState"
+    header = "# ProcID JobState"
+    header = "# ProcID Target ObsUTC JobState"
     arr = results_list
 
     # check file or screen output
@@ -134,13 +146,17 @@ if (len(results_list) > 0):
         outfile = open(outpath, "w")
         outfile.write("{0}\n".format(header))
         for x in range(0, len(arr)):
-            outfile.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(arr[x][0], arr[x][1], arr[x][2], arr[x][3], arr[x][4]))
+            #outfile.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(arr[x][0], arr[x][1], arr[x][2], arr[x][3], arr[x][4]))
+            #outfile.write("{0}\t{1}\n".format(arr[x][0], arr[x][1]))
+            outfile.write("{0}\t{1}\t{2}\t{3}\n".format(arr[x][0], arr[x][1], arr[x][2], arr[x][3]))
         outfile.close()
         print("{0} matching processing entries written to {1}.".format(len(arr), outpath))
     else:
         print (header)
         for x in range(0, len(arr)):
-            print ("{0}\t{1}\t{2}\t{3}\t{4}".format(arr[x][0], arr[x][1], arr[x][2], arr[x][3], arr[x][4]))
+            #print ("{0}\t{1}\t{2}\t{3}\t{4}".format(arr[x][0], arr[x][1], arr[x][2], arr[x][3], arr[x][4]))
+            #print ("{0}\t{1}".format(arr[x][0], arr[x][1]))
+            print ("{0}\t{1}\t{2}\t{3}".format(arr[x][0], arr[x][1], arr[x][2], arr[x][3]))
         print("{0} matching processing entries found.".format(len(arr)))
 
 else:
