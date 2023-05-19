cec0b32b12584fff3c5dd56f3807dca6f42540c0
NickSwainston <nickaswainston@gmail.com>
2023-05-19 15:49:41 +1000

    Made the Tsky models able to import that data from the repo

---
+++ meerpipe/data_load.py
@@ -0,0 +1,15 @@
+"""
+Loads all the data required by meerpipe from the data directory.
+"""
+
+import os
+
+datadir = os.path.join(os.path.dirname(__file__), 'data')
+
+# Load the files for simon's flux density radiometer equation calculations
+
+# Used for UHF Tsky calcs
+UHF_TSKY_FILE = os.path.join(datadir, 'UHF_Tsky.dat')
+
+# Used for LBAND Tsky calcs
+CHIPASS_EQU_CSV = os.path.join(datadir, 'CHIPASS_Equ.fits')
