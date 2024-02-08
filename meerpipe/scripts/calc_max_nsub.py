6b95da4dad9ef0ed31ed72d3cacab92147ea8acd
NickSwainston <nickaswainston@gmail.com>
2024-02-08 13:28:07 +1100

    Fixed the input args for the calc_max_nsb script

--- meerpipe/scripts/calc_max_nsub.py
+++ meerpipe/scripts/calc_max_nsub.py
@@ -7,25 +7,25 @@ def main():
     parser.add_argument(
         "--sn",
         type=float,
-        required=True
+        required=True,
         help="The signal-to-noise ratio of the archive",
     )
     parser.add_argument(
         "--nchan",
         type=int,
-        required=True
+        required=True,
         help="The number of frequency channels in the decimated archive",
     )
     parser.add_argument(
         "--duration",
         type=float,
-        required=True
+        required=True,
         help="The duration of the archive in seconds",
     )
     parser.add_argument(
         "--input_nsub",
         type=float,
-        required=True
+        required=True,
         help="The number of subintegrations of the input archive",
     )
     parser.add_argument(
@@ -43,14 +43,14 @@ def main():
     args = parser.parse_args()
 
     nsub = calc_max_nsub(
-        sn,
-        nchan,
-        duration,
-        input_nsub,
-        sn_desired=12.,
-        minimum_duration=480,
+        args.sn,
+        args.nchan,
+        args.duration,
+        args.input_nsub,
+        sn_desired=args.sn_desired,
+        minimum_duration=args.minimum_duration,
     )
-    if nsub = 1:
+    if nsub <= 1:
         print("1")
     else:
         print(f"1 {nsub}")
